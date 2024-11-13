93b4ea5ea26ffaaefcc65e0c09f02f56825b9861
MikeManzi <manzimike378@gmail.com>
2024-11-13 22:36:46 +0200

    chore: add the routes

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from 'react-router-dom';
+import SignupPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
+import Dashboard from './pages/Dashboard';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <AuthProvider>
+        <Layout />
+      </AuthProvider>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'signup',
+        element: <SignupPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'dashboard',
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
