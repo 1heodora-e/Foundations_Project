e9774bdb00835c74e22567d954545da24da4027d
MikeManzi <manzimike378@gmail.com>
2024-11-27 15:11:05 +0200

    refactor: remove comments

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -133,20 +133,6 @@ export default function SignupPage() {
                   />
                 </div>
               </div>
-              {/* <div>
-                <label className="block text-sm font-medium text-gray-700 mb-1">
-                  Confirm password
-                </label>
-                <div className="relative">
-                  <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
-                  <input
-                    type="password"
-                    {...register("confirmPassword")}
-                    className="pl-10 w-full rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                    required
-                  />
-                </div>
-              </div> */}
             </div>
 
             <div className="flex items-center justify-between">
